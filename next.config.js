30f67fc8f2fc5112b251c08a18d8e60ac76053b5
MicahRamirez <xaviermicahramirez@utexas.edu>
2019-12-08 17:25:28 +0900

    removing api key from static bundle, logging skip auth flag

--- next.config.js
+++ next.config.js
@@ -2,15 +2,14 @@ require("dotenv").config();
 const withCSS = require('@zeit/next-css');
 const webpack = require('webpack');
 
-const apiKey = JSON.stringify(process.env.SHOPIFY_API_KEY);
 const skipAuth = JSON.stringify(process.env.SKIP_AUTH);
+console.log(skipAuth);
 
 // next.config.js
 module.exports = withCSS({
   target: 'serverless',
   webpack: (config) => {
     const env = {
-      API_KEY: apiKey,
       SKIP_AUTH: skipAuth,
     };
     config.plugins.push(new webpack.DefinePlugin(env));
